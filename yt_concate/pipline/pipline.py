4ee567f78fe7b9889dab9bae6e6235c27d1e552a
NIKI <niki801226@hotmail.com>
2020-11-02 16:20:16 +0800

    Refactor Code into pipeline design pattern

---
+++ yt_concate/pipline/pipline.py
@@ -0,0 +1,17 @@
+from .steps.step import StepException
+
+
+class Pipline:
+    def __init__(self, steps):
+        self.steps = steps
+
+    # 不要笨笨的一個一個的去生成 可以用for loop
+    def run(self, inputs):
+        data = None
+        for step in self.steps:
+            try:
+                # 要接收上個步驟return的東西 接完在傳給下一個step ex.字幕()才拿得到list
+                data = step.process(data, inputs)
+            except StepException as e:
+                print('Exception happened:', e)
+                break
